fca53f85b1743116315a479e6eb821166168f870
Connor Leech <connorleech@gmail.com>
2015-04-23 20:11:31 -0700

    add gulp task on develop branch

--- play.js
+++ play.js
@@ -1,6 +1,15 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect');
 
+
+gulp.src('client/js/**/*.js') // Matches 'client/js/somedir/somefile.js' and resolves `base` to `client/js/`
+  .pipe(minify())
+  .pipe(gulp.dest('build'));  // Writes 'build/somedir/somefile.js'
+
+gulp.src('client/js/**/*.js', { base: 'client' })
+  .pipe(minify())
+  .pipe(gulp.dest('build'));  // Writes 'build/js/somedir/somefile.js'
+  
 gulp.task('webserver', function() {
   connect.server({
     livereload: true
