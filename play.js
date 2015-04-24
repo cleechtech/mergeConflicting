b6e98b1643cd5befa7303bdf0653e322f445bdd1
Connor Leech <connorleech@gmail.com>
2015-04-23 20:15:01 -0700

    make changes on master branch

--- play.js
+++ play.js
@@ -1,6 +1,13 @@
 var gulp = require('gulp'),
   connect = require('gulp-connect');
 
+// random changes on master branch
+gulp.src('./client/templates/*.jade')
+  .pipe(jade())
+  .pipe(gulp.dest('./build/templates'))
+  .pipe(minify())
+  .pipe(gulp.dest('./build/minified_templates'));
+
 gulp.task('webserver', function() {
   connect.server({
     livereload: true
